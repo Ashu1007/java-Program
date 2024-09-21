1ed69bdf5fe2dcb5256175565934fe52e364a75e
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2024-09-21 23:39:01 +0530

    edited

--- src/encapsulaton/Driver.java
+++ src/encapsulaton/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Employ e1=new Employ();
-		e1.setName("Tripathi ji...!");
+		e1.setName("Ashutosh Tripathi ji...!");
 		
 		System.out.println("=====================");
 		System.out.println("Name is:"+e1.getName());
